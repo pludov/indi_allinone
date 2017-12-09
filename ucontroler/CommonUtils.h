9ecd32aef5191543d1399d8ebd205e47a2252056
Ludovic P <pludov@nnx.com>
2017-12-09 19:35:00 +0100

    More parameters accepted by DEBUG macro

--- ucontroler/CommonUtils.h
+++ ucontroler/CommonUtils.h
@@ -26,6 +26,7 @@ void debugItem<const __FlashStringHelper*>(const __FlashStringHelper* t);/* {
 #define DEBUG3(A, B, C) { debugItem(A);debugItem(B);debugItem(C);   Serial1.write('\r');Serial1.write('\n');}
 #define DEBUG4(A, B, C, D) { debugItem(A);debugItem(B);debugItem(C);debugItem(D);   Serial1.write('\r');Serial1.write('\n');}
 #define DEBUG5(A, B, C, D, E) { debugItem(A);debugItem(B);debugItem(C);debugItem(D);debugItem(E);   Serial1.write('\r');Serial1.write('\n');}
+#define DEBUG6(A, B, C, D, E, F) { debugItem(A);debugItem(B);debugItem(C);debugItem(D);debugItem(E);debugItem(F);   Serial1.write('\r');Serial1.write('\n');}
 #else
 
 #include <iostream>
@@ -33,13 +34,13 @@ void debugItem<const __FlashStringHelper*>(const __FlashStringHelper* t);/* {
 #define DEBUG2(A, B) (std::cerr << A << B << "\n")
 #define DEBUG3(A, B, C) (std::cerr << A << B << C << "\n")
 #define DEBUG4(A, B, C, D) (std::cerr << A << B << C << D <<"\n")
-#define DEBUG4(A, B, C, D, E) (std::cerr << A << B << C << D << E <<"\n")
-#define DEBUG5(A, B, C, D, E, F) (std::cerr << A << B << C << D << E << F <<"\n")
+#define DEBUG5(A, B, C, D, E) (std::cerr << A << B << C << D << E <<"\n")
+#define DEBUG6(A, B, C, D, E, F) (std::cerr << A << B << C << D << E << F <<"\n")
 
 #define F(A) (A)
 #endif
 
-#define GET_DEBUG_MACRO(_1,_2,_3,_4,_5,NAME,...) NAME
-#define DEBUG(...) GET_DEBUG_MACRO(__VA_ARGS__, DEBUG5, DEBUG4, DEBUG3, DEBUG2, DEBUG1)(__VA_ARGS__)
+#define GET_DEBUG_MACRO(_1,_2,_3,_4,_5,_6, NAME,...) NAME
+#define DEBUG(...) GET_DEBUG_MACRO(__VA_ARGS__, DEBUG6, DEBUG5, DEBUG4, DEBUG3, DEBUG2, DEBUG1)(__VA_ARGS__)
 
 #endif
