4ffc05ecee7efaf9721a3a7a4bf16bc71810f425
Ludovic P <pludow@gmail.com>
2023-12-22 14:37:03 +0100

    Serialize debugging on multi-core

--- AstroController/include/CommonUtils.h
+++ AstroController/include/CommonUtils.h
@@ -31,13 +31,16 @@ void debugItem<const __FlashStringHelper*>(const __FlashStringHelper* t);/* {
 	}
 };*/
 
-#define DEBUG1(A) { debugItem(A);   SerialDbg.write('\r');SerialDbg.write('\n'); }
-#define DEBUG2(A, B) { debugItem(A);debugItem(B);   SerialDbg.write('\r');SerialDbg.write('\n'); }
-#define DEBUG3(A, B, C) { debugItem(A);debugItem(B);debugItem(C);   SerialDbg.write('\r');SerialDbg.write('\n'); }
-#define DEBUG4(A, B, C, D) { debugItem(A);debugItem(B);debugItem(C);debugItem(D);   SerialDbg.write('\r');SerialDbg.write('\n'); }
-#define DEBUG5(A, B, C, D, E) { debugItem(A);debugItem(B);debugItem(C);debugItem(D);debugItem(E);   SerialDbg.write('\r');SerialDbg.write('\n'); }
-#define DEBUG6(A, B, C, D, E, F) { debugItem(A);debugItem(B);debugItem(C);debugItem(D);debugItem(E);debugItem(F);   SerialDbg.write('\r');SerialDbg.write('\n'); }
-#define DEBUG7(A, B, C, D, E, F, G) { debugItem(A);debugItem(B);debugItem(C);debugItem(D);debugItem(E);debugItem(F);debugItem(G);   SerialDbg.write('\r');SerialDbg.write('\n'); }
+void debugOutputLock();
+void debugOutputUnlock();
+
+#define DEBUG1(A) { debugOutputLock(); debugItem(A);   SerialDbg.write('\r');SerialDbg.write('\n'); debugOutputUnlock(); }
+#define DEBUG2(A, B) { debugOutputLock(); debugItem(A);debugItem(B);   SerialDbg.write('\r');SerialDbg.write('\n'); debugOutputUnlock(); }
+#define DEBUG3(A, B, C) { debugOutputLock(); debugItem(A);debugItem(B);debugItem(C);   SerialDbg.write('\r');SerialDbg.write('\n'); debugOutputUnlock(); }
+#define DEBUG4(A, B, C, D) { debugOutputLock(); debugItem(A);debugItem(B);debugItem(C);debugItem(D);   SerialDbg.write('\r');SerialDbg.write('\n'); debugOutputUnlock(); }
+#define DEBUG5(A, B, C, D, E) { debugOutputLock(); debugItem(A);debugItem(B);debugItem(C);debugItem(D);debugItem(E);   SerialDbg.write('\r');SerialDbg.write('\n'); debugOutputUnlock(); }
+#define DEBUG6(A, B, C, D, E, F) { debugOutputLock(); debugItem(A);debugItem(B);debugItem(C);debugItem(D);debugItem(E);debugItem(F);   SerialDbg.write('\r');SerialDbg.write('\n'); debugOutputUnlock(); }
+#define DEBUG7(A, B, C, D, E, F, G) { debugOutputLock(); debugItem(A);debugItem(B);debugItem(C);debugItem(D);debugItem(E);debugItem(F);debugItem(G);   SerialDbg.write('\r');SerialDbg.write('\n'); debugOutputUnlock(); }
 
 #define GET_DEBUG_MACRO(_1,_2,_3,_4,_5,_6,_7, NAME,...) NAME
 #define DEBUG(...) GET_DEBUG_MACRO(__VA_ARGS__, DEBUG7, DEBUG6, DEBUG5, DEBUG4, DEBUG3, DEBUG2, DEBUG1)(__VA_ARGS__)
